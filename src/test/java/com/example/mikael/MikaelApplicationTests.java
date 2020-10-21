54642fc43d639166ae579da1c7907af07e147c0e
hua <616164124@qq.com>
2020-10-22 00:16:30 +0800

    2020年10月22日00:10:35

---
+++ src/test/java/com/example/mikael/MikaelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mikael;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MikaelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
